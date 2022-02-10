d181249baf6869e68b8416ef3b8d417de38bb333
jstearne <jared.stearne@gmail.com>
2022-02-10 14:11:14 -0800

    Updated controllers with index controller.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const indexCtrl = require('../controllers/index');
 
 
-router.get('/', function(req, res) {
-    res.render('home'); // route '/' now goes to views/home.ejs
-});
+router.get('/', indexCtrl.index);
 
 module.exports = router;
