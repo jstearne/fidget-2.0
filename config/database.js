50d8951009317f721d16bbf964ba99ed3ac80a2a
jstearne <jared.stearne@gmail.com>
2022-02-13 11:47:01 -0800

    Adding PassportJS and Atlas functionality. Needs to be Mongo Atlas with auth if I'm going to deploy.

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/movies', {
+mongoose.connect('mongodb://localhost/fidget', {
         useNewUrlParser: true, 
         useUnifiedTopology: true 
 });
--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const express = require('express');
 // connect to the database with Mongoose
 require('./config/database');
 
+// always in this order for the const variables
 const morgan = require('morgan');
+const session = require('express-session'); // always below morgan
+const passport = require('passport'); // always below session
 const PORT = process.env.PORT || 3000;
+
 const indexRouter = require('./routes/index');
 
 
@@ -19,11 +23,27 @@ app.set('view engine', 'ejs');
 // Mount middleware (app.use functions)
 app.use(morgan('dev'));
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// google strategy
+app.use(
+    session({
+        secret: "JaredIsCool", // whatever I want it to be
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+// app.use(session({... code above
+app.use(passport.initialize());
+app.use(passport.session());
+
+const methodOverride = require('method-override');
 
 // proper way of mounting routes 
-app.use('/', indexRouter);
+app.use('/', indexRouter); // this goes to HOME
 
-// home route
+// home route: redundant!
 app.get('/home', function(req, res) {
     res.render('home');
 });
