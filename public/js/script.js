e13fd086826ec2daa428ed5390784de2fa43eeeb
jstearne <jared.stearne@gmail.com>
2022-02-16 11:56:12 -0800

    Buttons have to be a href links, because I get unresolved routing errors with the form submit method. Buttons aren't very pretty, but the links have a bit of a halo for easier clicking, without ruining the layout on mobile. On hover reacts only to the link itself, so user doesn't click on edges and expect a result.

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,17 @@
 // alert("YO!")
+
+// function toAllFidgets() { 
+//     //   const gitHubLink = props.gitLink; 
+//     //   console.log(gitHubLink) // props always undefined. Use DOM to grab local button?
+    
+//     // could use conditional code here to get the right git repo
+//       window.open(
+//         `https://.com/jstearne`); 
+//         // `https://github.com/jstearne/${gitHubLink}`, "_blank"); 
+//     }
+    
+
+
+// <button className="navbar-button" onClick={toAllFidgets}>&nbsp;All Fidgets&nbsp;</button>
+// <button className="navbar-button" onClick={toNewFidget}>&nbsp;New Fidget&nbsp;</button>
+// <button className="navbar-button" onClick={toHome}>&nbsp;Return Home&nbsp;</button>
