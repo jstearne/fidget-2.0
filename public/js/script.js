a6a6705cb5b180486688ef7043d03477336d16b6
jstearne <jared.stearne@gmail.com>
2022-02-17 12:50:20 -0800

    Bizarre issue, can't change name of editPost function without breaking javascript. No idea why...

--- public/js/script.js
+++ public/js/script.js
@@ -3,11 +3,13 @@
 function toTop() { 
     window.scrollTo({top: 0, behavior: 'smooth'});
   }
-  
+  // hide buttons, show edit post field
   function editPost() {
     const editWindow = document.getElementById("edit-comment");
     const editButton = document.getElementById("edit-button");
+    const deleteForm = document.getElementById("delete-form");
     editWindow.style.display = "block"; 
     editButton.style.display = "none";
+    deleteForm.style.display = "none";
 };
   
