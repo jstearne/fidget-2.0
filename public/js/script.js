4ecf0dc07e9a66a816e8b003e024fd23e4d0e3ee
jstearne <jared.stearne@gmail.com>
2022-02-17 13:51:46 -0800

    Looks good, styling is done for a first draft. And now create comments doesn't work.

--- public/js/script.js
+++ public/js/script.js
@@ -2,14 +2,22 @@
 // scroll to top button
 function toTop() { 
     window.scrollTo({top: 0, behavior: 'smooth'});
-  }
-  // hide buttons, show edit post field
-  function editPost() {
-    const editWindow = document.getElementById("edit-comment");
-    const editButton = document.getElementById("edit-button");
-    const deleteForm = document.getElementById("delete-form");
-    editWindow.style.display = "block"; 
-    editButton.style.display = "none";
-    deleteForm.style.display = "none";
+}
+// hide buttons, show edit post field
+function editPost() {
+  const editWindow = document.getElementById("edit-comment");
+  const editButton = document.getElementById("edit-button");
+  const deleteForm = document.getElementById("delete-form");
+  editWindow.style.display = "block"; 
+  editButton.style.display = "none";
+  deleteForm.style.display = "none";
 };
-  
+
+function cancelEdit() {
+  const editWindow = document.getElementById("edit-comment");
+  const editButton = document.getElementById("edit-button");
+  const deleteForm = document.getElementById("delete-form");
+  editWindow.style.display = "none"; 
+  editButton.style.display = "block";
+  deleteForm.style.display = "block";
+};
